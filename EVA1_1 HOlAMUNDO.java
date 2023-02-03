51218ea633c199c1679622859d2c611ea9c587c0
JADLCB <124391638+JADLCB@users.noreply.github.com>
2023-02-03 12:40:22 -0600

    Add files via upload

---
+++ EVA1_1 HOlAMUNDO.java
@@ -0,0 +1,13 @@
+
+public class EVA1_1_HOlAMUNDO {
+
+     
+    public static void main(String args[]) {
+        int x =100;
+  
+        System.out.println("HOLA MUNDO ! !");
+    }
+    public static void algo(){
+      System.out.println(x);
+    }
+}
