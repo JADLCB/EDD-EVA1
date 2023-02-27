308ab4b531932277f5f29113c8eaa3918ce4454c
JADLCB <124391638+JADLCB@users.noreply.github.com>
2023-02-27 00:20:08 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE3/src/eva1_14_alcance3/EVA1_14_ALCANCE3.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_14_alcance3;
+
+/**
+ *
+ * @author ProBook
+ */
+public class EVA1_14_ALCANCE3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=100;
+        System.out.println(x);
+        for (int i = 0; i < 10; i++) {
+            //int x=100;//no se puede,existe en el main
+            System.out.println(i);
+            System.out.println(x);
+            if (i == 5){
+                System.out.println(x);
+            }
+            
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        
+    }
+    
+    public static void Algo(int x){
+
+}
+public static void Algo2(int x){
+
+}
+}
+
