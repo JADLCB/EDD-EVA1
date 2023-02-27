ce43ceaa42e3f3187eeb8b009ebba5a937d9e2e3
JADLCB <124391638+JADLCB@users.noreply.github.com>
2023-02-27 00:15:23 -0600

    Add files via upload

---
+++ EVA_1_METODO_1/src/main/java/com/mycompany/eva_1_metodo_1/EVA1_7_METODO_1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva_1_metodo_1;
+
+/**
+ *
+ * @author ProBook
+ *///Square escuando elevasalcuadrado
+public class EVA1_7_METODO_1 {
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+    public static void myMethod(){
+        System.out.println("You have called me My name is myMethod");
+                
+    }
+    
+}
