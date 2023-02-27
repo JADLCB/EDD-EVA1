7eafb078622bf3c6729e0b1452a2763b0abf7341
JADLCB <124391638+JADLCB@users.noreply.github.com>
2023-02-27 00:18:45 -0600

    Add files via upload

---
+++ EVA1_11_CALIFAS/src/main/java/com/mycompany/eva1_11_califas/EVA1_11_CALIFAS.java
@@ -0,0 +1,20 @@
+package com.mycompany.eva1_11_califas;
+
+public class EVA1_11_CALIFAS {
+
+    public static void main(String[] args) {
+        String resu;
+        resu = calificar(70);
+        System.out.println(resu);
+        System.out.println(calificar 70);
+    }
+     public static String calificar(int califa){
+         if((califa>=91)&&(califa<=100)){
+         }else if((califa>=81)&&(califa<90)){
+         }else if((califa>=71)&&(califa<=80)){
+         }else{
+         }
+        return null;
+    
+    }
+}
