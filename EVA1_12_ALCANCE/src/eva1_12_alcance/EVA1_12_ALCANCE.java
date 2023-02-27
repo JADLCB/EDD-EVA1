5fb10fd2bed614413ffc9dbce8d3e92d406757ae
JADLCB <124391638+JADLCB@users.noreply.github.com>
2023-02-27 00:19:10 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_12_alcance;
+
+/**
+ *
+ * @author ProBook
+ */
+public class EVA1_12_ALCANCE {
+
+   
+    public static void main(String[] args) {
+        System.out.println(x);//no exite la x, porque esta declarada despues de la funion
+        
+        System.out.println(args);//args esta declarando antes de la funsion
+      int x=100;
+        System.out.println(x);
+    }
+   
+}
