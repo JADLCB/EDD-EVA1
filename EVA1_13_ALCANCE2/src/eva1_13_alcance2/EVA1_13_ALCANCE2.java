e0f578c59d937a9b2d08bc19a2e89a68a46dbae5
JADLCB <124391638+JADLCB@users.noreply.github.com>
2023-02-27 00:19:35 -0600

    Add files via upload

---
+++ EVA1_13_ALCANCE2/src/eva1_13_alcance2/EVA1_13_ALCANCE2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_13_alcance2;
+
+/**
+ *
+ * @author ProBook
+ */
+public class EVA1_13_ALCANCE2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(x);
+            int x=100;
+            System.out.println(x);
+            {
+                int y=1000;
+                System.out.println(y);
+            }
+            System.out.println(y);
+        }
+        System.out.println(i);
+            System.out.println(x);
+    }
+    
+}
